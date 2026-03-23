f6ee87a0114bbc2b7b29c1b013f521cb1bc34a83
Hoda Jafari <h.jafari@edu.bbq.de>
2026-03-23 14:52:40 +0100

    add dj-database-url

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
